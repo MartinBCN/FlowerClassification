de0397c8c1ed1e5f88a667f0f0fcffbdc047e20f
martin <martin-cleven@gmx.net>
2021-07-29 20:08:10 +0200

    Early draft

---
+++ flower_classification/flower_classifier.py
@@ -0,0 +1,116 @@
+from abc import ABC
+from pathlib import Path
+from typing import Union
+import torch
+from torch.nn import Module, L1Loss, MSELoss, SmoothL1Loss
+from torch.optim import Adam, SGD
+import matplotlib.pyplot as plt
+from torch.optim.lr_scheduler import StepLR
+import numpy as np
+
+plt.style.use('ggplot')
+
+
+class FlowerClassifier:
+    optimizers = {'adam': Adam, 'sgd': SGD}
+    criterions = {'l1': L1Loss, 'smooth_l1': SmoothL1Loss, 'mse': MSELoss}
+    scheduler_choices = {'steplr': StepLR}
+
+    def __init__(self):
+
+        self.model = self.get_model()
+        self.criterion = None
+        self.optimizer = None
+        self.__optimizer_choice = None
+        self.scheduler = None
+        self.scheduler_parameter = None
+        self.hyper_parameter = {}
+
+        self.training_log = {'train': {'batch_loss': [], 'epoch_loss': [], 'batch_accuracy': [], 'epoch_accuracy': []},
+                             'validation': {'batch_loss': [], 'epoch_loss': [], 'batch_accuracy': [],
+                                            'epoch_accuracy': []},
+                             'learning_rate': []}
+
+    def get_model(self) -> Module:
+        pass
+
+    def set_criterion(self, criterion: str) -> None:
+        self.criterion = self.criterions[criterion]()
+
+    def set_optimizer(self, optimizer: str, hyper_parameter: dict = None) -> None:
+        if hyper_parameter is None:
+            hyper_parameter = {}
+        self.optimizer = self.optimizers[optimizer](self.model.parameters(), **hyper_parameter)
+        self.hyper_parameter = hyper_parameter
+        self.__optimizer_choice = optimizer
+
+    def set_scheduler(self, scheduler: str, params: dict):
+        self.scheduler = self.scheduler_choices[scheduler](self.optimizer, **params)
+        self.scheduler_parameter = params
+
+    def get_lr(self):
+        for param_group in self.optimizer.param_groups:
+            return param_group['lr']
+
+    @classmethod
+    def load(cls, filepath: Union[str, Path]):
+        state = torch.load(filepath)
+
+        new = cls()
+
+        new.set_optimizer(state['optimizer'], state['hyper_parameter'])
+
+        new.model.load_state_dict(state['model_state_dict'])
+        new.optimizer.load_state_dict(state['optimizer_state_dict'])
+        new.training_log = state['training_log']
+
+        return new
+
+    def save(self, filepath: Union[str, Path]):
+
+        state = {'model_state_dict': self.model.state_dict(),
+                 'optimizer_state_dict': self.optimizer.state_dict(),
+                 'training_log': self.training_log,
+                 'optimizer': self.__optimizer_choice,
+                 'hyper_parameter': self.hyper_parameter
+                 }
+
+        torch.save(state, filepath)
+
+    def get_inference_model(self):
+        return self.model
+
+    def plot(self, filepath: Union[str, Path] = None):
+
+        fig, axes = plt.subplots(2, 2, figsize=(20, 10))
+
+        train_batch_loss = self.training_log['train']['batch_loss']
+        axes[0, 0].plot(np.convolve(train_batch_loss, np.ones(10)/10, mode='same'), label='Train', color='red')
+        axes[0, 0].plot(train_batch_loss, label='Train', color='red', alpha=0.2)
+        val_batch_loss = self.training_log['validation']['batch_loss']
+        axes[0, 0].plot(np.convolve(val_batch_loss, np.ones(10)/10, mode='same'), label='Train', color='blue')
+        axes[0, 0].plot(val_batch_loss, label='Validation', color='blue', alpha=0.2)
+        axes[0, 0].legend(title='Batch Loss')
+
+        axes[0, 1].plot(self.training_log['train']['epoch_loss'], label='Train')
+        axes[0, 1].plot(self.training_log['validation']['epoch_loss'], label='Validation')
+        axes[0, 1].legend(title='Epoch Loss')
+
+        train_batch_accuracy = self.training_log['train']['batch_accuracy']
+        axes[1, 0].plot(np.convolve(train_batch_accuracy, np.ones(10)/10, mode='same'), label='Train', color='red')
+        axes[1, 0].plot(train_batch_accuracy, label='Train', color='red', alpha=0.2)
+        val_batch_accuracy = self.training_log['validation']['batch_accuracy']
+        axes[1, 0].plot(np.convolve(val_batch_accuracy, np.ones(10)/10, mode='same'), label='Train', color='blue')
+        axes[1, 0].plot(val_batch_accuracy, label='Validation', color='blue', alpha=0.2)
+        axes[1, 0].legend(title='Batch accuracy')
+        axes[1, 0].set_ylim(-0.2, 1.0)
+
+        axes[1, 1].plot(self.training_log['train']['epoch_accuracy'], label='Train')
+        axes[1, 1].plot(self.training_log['validation']['epoch_accuracy'], label='Validation')
+        axes[1, 1].legend(title='Epoch accuracy')
+        axes[1, 1].set_ylim(-0.2, 1.0)
+
+        if filepath is None:
+            plt.show()
+        else:
+            plt.savefig(filepath)
---
+++ flower_classification/trainer.py
@@ -0,0 +1,115 @@
+from datetime import datetime
+import numpy as np
+import torch
+from sklearn.metrics import r2_score
+from torch.utils.data import DataLoader
+from datetime import datetime
+import logging
+
+from flower_classification.flower_classifier import FlowerClassifier
+
+logger = logging.getLogger()
+
+
+class FlowerTrainer(FlowerClassifier):
+
+    def __init__(self):
+        super().__init__()
+
+    def evaluate(self, data_loader: DataLoader):
+
+        predicted_labels = []
+        ground_truth = []
+
+        for i, (image, label) in enumerate(data_loader):
+
+            with torch.no_grad():
+
+                predicted_labels.append(self.model.predict(image).detach().cpu().numpy())
+                ground_truth.append(label.detach().cpu().numpy())
+
+        # Log accuracy
+        predicted_labels = np.concatenate(predicted_labels)
+        ground_truth = np.concatenate(ground_truth)
+        accuracy = (ground_truth == predicted_labels).mean()
+        return accuracy, predicted_labels, ground_truth
+
+    def train(self, data_loader: dict, epochs: int, early_stop_epochs: int = 5):
+
+        start = datetime.now()
+        for epoch in range(epochs):
+            logger.info(('=' * 125))
+            logger.info(f'Epoch {epoch}')
+            logger.info(('=' * 125))
+
+            self.training_log['learning_rate'].append(self.get_lr())
+
+            for phase in ['train', 'validation']:
+
+                logger.info(('-' * 125))
+                logger.info(f'Phase {phase}')
+                logger.info(('-' * 125))
+
+                if phase == 'train':
+                    self.model.train()
+                else:
+                    self.model.eval()
+
+                if phase not in self.training_log.keys():
+                    self.training_log[phase] = {'epoch_loss': [], 'batch_loss': []}
+
+                epoch_loss = 0
+                epoch_ground_truth = []
+                epoch_predicted_labels = []
+                batch_time = []
+
+                for i, sample in enumerate(data_loader[phase]):
+                    start_batch = datetime.now()
+                    image = sample['image']
+                    label = sample['keypoints']
+                    if phase == 'train':
+                        # zero the parameter gradients
+                        self.optimizer.zero_grad()
+
+                    # forward + backward + optimize
+                    outputs = self.model(image)
+                    loss = self.criterion(outputs, label.float())
+                    epoch_loss += loss.detach().cpu().numpy()
+
+                    # Batch Accuracy
+                    batch_predicted_labels = outputs.detach().cpu().numpy().reshape(-1)
+                    batch_true_labels = label.detach().cpu().numpy().reshape(-1)
+                    epoch_predicted_labels.append(batch_predicted_labels)
+                    epoch_ground_truth.append(batch_true_labels)
+
+                    if phase == 'train':
+                        loss.backward()
+                        self.optimizer.step()
+
+                    # Permanent Value Tracking
+                    batch_loss = loss.detach().cpu().numpy()
+                    self.training_log[phase]['batch_loss'].append(batch_loss)
+                    accuracy = r2_score(batch_true_labels, batch_predicted_labels)
+                    self.training_log[phase]['batch_accuracy'].append(accuracy)
+
+                    # Regular Logging
+                    log_string = f'Epoch {epoch + 1}/{epochs}, Batch {i+1}/{len(data_loader[phase])}'
+                    log_string += f', Mean Epoch Loss: {epoch_loss / (i + 1):.2f}'
+                    log_string += f'| Batch Loss: {batch_loss:.2f}'
+                    current_accuracy = r2_score(np.concatenate(epoch_ground_truth),
+                                                np.concatenate(epoch_predicted_labels))
+                    log_string += f' |Epoch Accuracy: {current_accuracy:.2f}'
+                    log_string += f'| Batch Accuracy: {accuracy:.2f}'
+                    delta = (datetime.now() - start_batch).microseconds/image.shape[0]/1000
+                    batch_time.append(delta)
+                    log_string += f' | Time/image: {np.mean(batch_time):.2f}ms '
+
+                    logger.info(log_string)
+
+                # Log Epoch (accuracy and loss)
+                accuracy = r2_score(np.concatenate(epoch_ground_truth), np.concatenate(epoch_predicted_labels))
+                self.training_log[phase]['epoch_accuracy'].append(accuracy)
+                self.training_log[phase]['epoch_loss'].append(epoch_loss)
+
+            if self.scheduler is not None:
+                self.scheduler.step()
